176f76c5478b6043224403123fa686c05bccd323
rahadians <matrix.black82@gmail.com>
2022-05-06 10:58:16 +0700

    berhasil ambil gambar dari memory

--- lib/app/modules/addnews/controllers/addnews_controller.dart
+++ lib/app/modules/addnews/controllers/addnews_controller.dart
@@ -1,13 +1,15 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:image_picker/image_picker.dart';
-// import 'package:image_picker/image_picker.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-// import 'package:file_picker/file_picker.dart';
 
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 import '../../../assets/models/constant.dart';
 
 class AddnewsController extends GetxController {
@@ -15,13 +17,17 @@ class AddnewsController extends GetxController {
 
   RxString avatarUrl = "".obs;
   RxString imageUrl = "".obs;
+  RxString device = "".obs;
+  RxString filepath = "".obs;
 
   RxString selectedImagePath = ''.obs;
   RxString selectedImageName = ''.obs;
   RxString selectedImageSize = ''.obs;
   RxString no_id = "".obs;
+  RxString animationController = "".obs;
 
-  // late XFile? ambilGambar;
+  late Uint8List filebytes;
+
   late File xfile;
 
   File? ambilGambar;
@@ -37,38 +43,68 @@ class AddnewsController extends GetxController {
   //     SupabaseClient(BaseUrl.cBaseUrl, BaseUrl.cAnonKey);
   SupabaseClient client = Supabase.instance.client;
 
-  Future<void> upload() async {
+  Future<void> upload(ImageSource imageSource) async {
     final picker = ImagePicker();
     final imageFile = await picker.pickImage(
-      source: ImageSource.camera,
+      source: (imageSource == ImageSource.camera)
+          ? ImageSource.camera
+          : ImageSource.gallery,
       maxWidth: 200,
       maxHeight: 200,
     );
     if (imageFile == null) {
       return;
+      print("gagal");
     }
 
     isLoading.value = true;
+
+    File tmpFile = File(imageFile.path);
 
     final bytes = await imageFile.readAsBytes();
     final fileExt = imageFile.path.split('.').last;
     final fileName = '${DateTime.now().toIso8601String()}.$fileExt';
     final filePath = fileName;
-    final response =
-        await client.storage.from('images').uploadBinary(filePath, bytes);
+    filepath.value = filePath.toString();
+    filebytes = bytes;
+
+    final Directory extDir = await getApplicationDocumentsDirectory();
+    String dirPath = extDir.path;
+    final String extfilePath = '$dirPath/$fileName';
+
+    final File newImage = await tmpFile.copy(extfilePath);
+
+    if (imageFile != null) {
+      tmpFile = newImage;
+      print("berhasil");
+    } else {
+      print('No image selected.');
+    }
+
+    imageUrl.value = newImage.toString();
+    print("========");
+    print("${bytes}");
+    print(extfilePath);
+
+    // print(imageUrl.value);
+
+    // final response =
+    //     await client.storage.from('images/temp').uploadBinary(filePath, bytes);
 
     isLoading.value = false;
 
-    final error = response.error;
-    if (error != null) {
-      Get.snackbar("error", error.message);
+    // final error = response.error;
+    // if (error != null) {
+    //   Get.snackbar("error", error.message);
 
-      return;
-    }
-    StorageResponse<String> imageUrlResponse =
-        client.storage.from('images').getPublicUrl(filePath);
+    //   return;
+    // }
 
-    imageUrl.value = imageUrlResponse.data.toString();
+    // StorageResponse<String> imageUrlResponse =
+    //     client.storage.from('images/temp').getPublicUrl(filePath);
+
+    // imageUrl.value = imageUrlResponse.data.toString();
+    // imageUrl.value = imageFile.name;
   }
 
   simpan() async {
@@ -80,13 +116,32 @@ class AddnewsController extends GetxController {
     isLoading.value = true;
 
     try {
-      await client.from("profiles").insert({
-        "id": client.auth.currentUser!.id,
-        "username": usernameC.text,
-        "website": websiteC.text,
-        "avatar_url": imageUrl.value.toString(),
-        "updated_at": DateTime.now().toIso8601String()
-      }).execute();
+      print("simpanyyee");
+      // final response = await client.storage
+      //     .from('images/images')
+      //     .uploadBinary(filepath.value, filebytes!);
+
+      print(filepath.value);
+
+      final res = await client.storage
+          .from('images/temp')
+          .remove(['2022-05-05T17:52:34.254481.jpg']);
+
+      final resti =
+          await client.storage.from('avatars').remove(['avatar1.jpg']);
+
+      // StorageResponse<String> imageUrlResponse =
+      //     client.storage.from('images/images').getPublicUrl(filepath.value);
+
+      // await client.from("profiles").insert({
+      //   "id": client.auth.currentUser!.id,
+      //   "username": usernameC.text,
+      //   "website": websiteC.text,
+      //   "avatar_url": imageUrl.value.toString(),
+      //   "updated_at": DateTime.now().toIso8601String()
+      // }).execute();
+
+      // final response = imageFile;
 
       Get.snackbar("Info", "Data Sudah Tersimpan",
           duration: Duration(seconds: 3));
@@ -95,9 +150,10 @@ class AddnewsController extends GetxController {
     }
 
     isLoading.value = false;
-    // Get.back();
-    // } else {
-    //   Get.snackbar("error", "knapa salah", duration: Duration(seconds: 3));
-    // }
+  }
+
+  Future showToast(String message) async {
+    await Fluttertoast.cancel();
+    Fluttertoast.showToast(msg: message, fontSize: 18);
   }
 }
--- lib/app/modules/addnews/views/addnews_view.dart
+++ lib/app/modules/addnews/views/addnews_view.dart
@@ -8,40 +8,102 @@ import 'package:image_picker/image_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../../assets/models/constant.dart';
 import '../controllers/addnews_controller.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 // "https://mnxjbshqqxhbadvuazxt.supabase.co/storage/v1/object/public/images/Mango Mojito.jpg",
 SupabaseClient client = Supabase.instance.client;
 
 class AddnewsView extends GetView<AddnewsController> {
+  ValueNotifier<bool> isDialOpen = ValueNotifier(false);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        print(client.auth.currentUser!.id.runtimeType);
-      }),
+      floatingActionButton:
+          FloatingActionButton(heroTag: "btnbesar", onPressed: () {}),
       appBar: AppBar(),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(children: [
-            InkWell(
-                onTap: () => controller.upload(),
-                child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Obx(() => (controller.imageUrl.value == "" ||
-                            controller.imageUrl.value == "")
-                        ? Container(
-                            width: double.infinity,
-                            height: 200,
-                            child: Text("oOk"))
-                        : Container(
-                            width: double.infinity,
-                            height: 200,
-                            child: Image.network(
-                              controller.imageUrl.value,
-                              width: 150,
-                              height: 150,
-                              fit: BoxFit.cover,
-                            ))))),
+            Container(
+              // onTap: () => controller.upload(),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Obx(
+                      () => (controller.imageUrl.value == "" ||
+                              controller.imageUrl.value == "")
+                          ? Container(
+                              width: double.infinity,
+                              height: 200,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.asset(
+                                  "lib/app/assets/img/dummyimage.png",
+                                  // "/data/user/0/com.example.owl_flutter/app_flutter/2022-05-06T10:32:23.274935.jpg",
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: kLightgreen)),
+                            )
+                          : Container(
+                              width: double.infinity,
+                              height: 250,
+                              child: (controller.filebytes != null)
+                                  ? Image.memory(
+                                      controller.filebytes,
+                                      fit: BoxFit.fill,
+                                    )
+                                  : Image.asset(
+                                      "lib/app/assets/img/girlimage.png"),
+                            ),
+                    ),
+                  ),
+                  Stack(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20, bottom: 10),
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: FloatingActionButton(
+                            heroTag: "btnkecil",
+                            autofocus: true,
+                            mini: false,
+                            onPressed: () {},
+                            child: SpeedDial(
+                              icon: Icons.add_a_photo,
+                              // buttonSize: Size(10, 10),
+                              backgroundColor: kDarkerGreen,
+                              spacing: 5,
+                              spaceBetweenChildren: 5,
+                              closeManually: false,
+                              children: [
+                                SpeedDialChild(
+                                    onTap: () {
+                                      controller.upload(ImageSource.camera);
+                                    },
+                                    child: Icon(Icons.camera_alt)),
+                                SpeedDialChild(
+                                  onTap: () {
+                                    controller.upload(ImageSource.gallery);
+                                  },
+                                  child: Icon(Icons.photo_album_outlined),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
             TextFormField(
               controller: controller.usernameC,
               keyboardType: TextInputType.text,
@@ -70,23 +132,6 @@ class AddnewsView extends GetView<AddnewsController> {
             SizedBox(
               height: 20,
             ),
-            // TextFormField(
-            //   controller: controller.descC,
-            //   autocorrect: false,
-            //   keyboardType: TextInputType.emailAddress,
-            //   validator: (value) {
-            //     if (value!.isEmpty) {
-            //       return "Masukkan Email Anda";
-            //     } else {
-            //       return null;
-            //     }
-            //   },
-            //   // onSaved: (value) => controller.descC.value = value!,
-            //   onChanged: (value) {
-            //     // controller.descC.value = value;
-            //   },
-            //   decoration: kTextFieldDecoration.copyWith(hintText: 'Email'),
-            // ),
             ElevatedButton(
                 onPressed: () {
                   controller.simpan();
